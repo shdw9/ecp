c08a2ea7d0df9bc20f76e1245d8b9bc1b05813c8
CK <73201351+shdw9@users.noreply.github.com>
2022-12-08 15:23:51 -0800

    Add files via upload

---
+++ dgemv-openmp-nested.cpp
@@ -0,0 +1,38 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+
+const char* dgemv_desc = "Nested OpenMP dgemv.";
+
+/*
+ * This routine performs a dgemv operation
+ * Y :=  A * X + Y
+ * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
+ * On exit, A and X maintain their input values.
+ */
+
+void my_dgemv(int n, double* A, double* x, double* y) {
+
+   #pragma omp parallel
+   {
+      int nthreads = omp_get_num_threads();
+      int thread_id = omp_get_thread_num();
+      printf("Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
+   }
+
+   // insert your dgemv code here. you may need to create additional parallel regions,
+   // and you may want to comment out the above parallel code block that prints out
+   // nthreads and thread_id so as to not taint your timings
+
+   #pragma omp parallel for
+   // insert your code here: implementation of basic matrix multiply
+   for(int i = 0; i < n; i++) {
+         #pragma omp parallel for
+         for(int j = 0; j < n; j++) {
+                y[i] += A[i * n + j] * x[j];
+         }
+   }
+
+}
+
